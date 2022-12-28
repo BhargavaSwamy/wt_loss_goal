37038a0e38fcf63771a6db5fd8f2ebdbb505f563
Bhargava Swamy <netmedico@gmail.com>
2022-12-28 11:20:09 +0000

    "Removed default test"

--- test/bmi_calc_web/controllers/page_controller_test.exs
+++ test/bmi_calc_web/controllers/page_controller_test.exs
@@ -1,8 +1,4 @@
 defmodule BmiCalcWeb.PageControllerTest do
   use BmiCalcWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
 end
